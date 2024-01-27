a90ca6c655ea30330c11756648a40bbc4f32de4a
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-28 03:34:20 +0500

    commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -46,7 +46,7 @@ const {data:session} = useSession();
                      Sign Out
                </button>
                <Link href="/profile">
-                <Image src="/assets/images/logo.svg"
+                <Image src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -75,8 +75,9 @@ const {data:session} = useSession();
                 )}
             </div>
 <div className="sm:hidden flex relative">
-    {session?.user ? (<div className="flex">
-        <Image src="/assets/images/logo.svg"
+    {session?.user ?
+     (<div className="flex">
+        <Image src={session?.user.image}
             width={37}
             height={37}
                 className="rounded-full"
